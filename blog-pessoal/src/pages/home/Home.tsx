4d36fccf8259ca2c790adca5b586b9e100de6abf
tadyello <tadyello@hotmail.com>
2022-11-23 12:55:56 -0300

    Criação de um complemento.

---
+++ blog-pessoal/src/pages/home/Home.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import './Home.css'
+
+function Home() {
+    return (
+        <div>
+            <h1 className="fonte">Tadyêllo Bastos</h1>
+            <p>Solteiro(a)-01/05/1994<br />Rua Soldado Sebastião Machado 45, Jacarepaguá, Rio de Janeiro<br />+55 21 99554-3233<br />tadyello@hotmail.com</p>
+            <p>OBJETIVO:</p>
+            <p>Cargo de Analista de </p>
+            <p>FORMAÇÃO ACADÊMICA:</p>
+            <p>Graduação em Análise e Desenvolvimento de Sistemasddsasa.<a href="https://www.mackenzie.br/">Universidade Presbiteriana Mackenzie</a>, Rio de Janeiro,2022-2024</p>
+            <p>EXPERIÊNCIA PROFISSIONAL:</p>
+            <p>11/2014-10/2021-LEROY MERLIN BRASIL</p>
+            <ul>
+                <li> Cargo: Vendedor de Projetos</li>
+                <li> Principais Atividades: Venda e Atendimento ao Cliente</li>
+                <li>Programador</li>
+            </ul>
+            <p>IDIOMAS:</p>
+            <ul>
+                <li>Inglês básico.</li>
+            </ul>    
+            
+        </div>
+    );
+}
+export default Home;
